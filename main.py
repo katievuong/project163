10ffa930cfcdc11bacf941405adffb003aa34dd3
katievuong <kvuong1@uw.edu>
2023-03-02 19:46:05 -0800

    Moved main method to test file, import pandas

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
 import pandas as pd
 
 
-def main():
-    priv_rights_data = pd.read_csv("PRC Data Breach Chronology - 1.13.20.csv")
-    vincent_data = pd.read_csv("breaches.csv")
-
-
-if __name__ == '__main__':
-    main()
