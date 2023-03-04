ead4fa907c3aeadd22ae35deb3b06a3485b7cd73
katievuong <kvuong1@uw.edu>
2023-03-03 19:07:53 -0800

    add split dates function

--- cleanup.py
+++ cleanup.py
@@ -7,3 +7,10 @@ def prc_unique_breaches(data: pd.DataFrame) -> list[str]:
 
 def vin_unique_breaches(data: pd.DataFrame) -> list[str]:
     return data["Type_of_Breach"].unique()
+
+
+def split_dates(data: pd.DataFrame) -> None:
+    data["Date_Posted_or_Updated"] = (data["Date_Posted_or_Updated"].str.split(
+                                      "-"))
+    data["breach_start"] = data["breach_start"].str.split("-")
+    return data
--- test.py
+++ test.py
@@ -10,12 +10,17 @@ def test_vin_unique_breaches(data: pd.DataFrame) -> list[str]:
     print(cleanup.vin_unique_breaches(data))
 
 
+def test_split_dates(data: pd.DataFrame) -> pd.DataFrame:
+    print(cleanup.split_dates(data))
+
+
 def main():
     priv_rights_data = pd.read_csv("PRC Data Breach Chronology - 1.13.20.csv")
     vincent_data = pd.read_csv("breaches.csv")
 
-    test_unique_breaches(priv_rights_data)
-    test_vin_unique_breaches(vincent_data)
+    # test_unique_breaches(priv_rights_data)
+    # test_vin_unique_breaches(vincent_data)
+    test_split_dates(vincent_data)
 
 
 if __name__ == '__main__':
