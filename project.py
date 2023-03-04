e24320a828848100e1f6008322ac9e9d87f4a093
katievuong <kvuong1@uw.edu>
2023-03-03 16:29:38 -0800

    added function skeletons

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 
 
 def prc_unique_breaches(data: pd.DataFrame) -> list[str]:
@@ -7,3 +8,15 @@ def prc_unique_breaches(data: pd.DataFrame) -> list[str]:
 
 def vin_unique_breaches(data: pd.DataFrame) -> list[str]:
     return data["Type_of_Breach"].unique()
+
+
+def plot_trend_line(data: pd.DataFrame) -> None:
+    # fig = px.line(data, x="", y="", title="")
+    # fig.show()
+    pass
+
+
+def plot_breach_types(data: pd.DataFrame) -> None:
+    # fig = px.pie(data, values="", names="", title="")
+    # fig.show()
+    pass
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def test_vin_unique_breaches(data: pd.DataFrame) -> list[str]:
     print(project.vin_unique_breaches(data))
 
 
-def code():
+def main():
     priv_rights_data = pd.read_csv("PRC Data Breach Chronology - 1.13.20.csv")
     vincent_data = pd.read_csv("breaches.csv")
 
@@ -19,4 +19,4 @@ def code():
 
 
 if __name__ == '__main__':
-    code()
+    main()
