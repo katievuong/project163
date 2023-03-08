05308448d9c0bfcc2df79986f596654d2ec5d436
tyrellgarza <otgarza@uw.edu>
2023-03-07 18:08:29 -0800

    added second data set in orange

--- plots.py
+++ plots.py
@@ -4,26 +4,47 @@ import plotly.graph_objects as go
 import pandas as pd
 
 
-def plot_breaches_per_year(data: pd.DataFrame) -> None:
-    data = data[data['year'].between(2009, 2013)]  # filter by year
-    year_counts = data['year'].value_counts().sort_index()
+def plot_breaches_per_year(data1: pd.DataFrame, data2: pd.DataFrame) -> None:
+    # filter data by year
+    data1 = data1[data1['year'].between(2009, 2017)]
+    data2 = data2[data2['Year of Breach'].between(2009, 2017)]
+
+    # count number of breaches per year for each dataset
+    year_counts1 = data1['year'].value_counts().sort_index()
+    year_counts2 = data2['Year of Breach'].value_counts().sort_index()
 
     # create plot data
-    trace = go.Scatter(x=year_counts.index,
-                       y=year_counts.values,
-                       mode='lines+markers',
-                       name='Number of Breaches',
-                       line=dict(color='#007bff'),
-                       marker=dict(color='#007bff'))
+    trace1 = go.Scatter(
+        x=year_counts1.index,
+        y=year_counts1.values,
+        mode='lines+markers',
+        name='Breaches in Dataset 1',
+        line=dict(color='#007bff'),
+        marker=dict(color='#007bff')
+    )
+
+    trace2 = go.Scatter(
+        x=year_counts2.index,
+        y=year_counts2.values,
+        mode='lines+markers',
+        name='Breaches in Dataset 2',
+        line=dict(color='#ff7f0e'),
+        marker=dict(color='#ff7f0e')
+    )
 
     # create layout
-    layout = go.Layout(title='Recent Data Breach Trends Over Time',
-                       xaxis=dict(title='Year',
-                                  tickvals=[2009, 2010, 2011, 2012, 2013]),
-                       yaxis=dict(title='Number of Breaches'))
+    layout = go.Layout(
+        title='Recent Data Breach Trends Over Time',
+        xaxis=dict(
+            title='Year',
+            tickvals=[2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+            range=[2009, 2017]
+        ),
+        yaxis=dict(title='Number of Breaches')
+    )
 
     # create figure and plot
-    fig = go.Figure(data=[trace], layout=layout)
+    fig = go.Figure(data=[trace1, trace2], layout=layout)
     fig.show()
 
 
@@ -50,8 +71,9 @@ def plot_breach_types(data: pd.DataFrame) -> None:
 
 
 def main():
-    df = pd.read_csv("breaches.csv")
-    plot_breaches_per_year(df)
+    df1 = pd.read_csv("breaches.csv")
+    df2 = pd.read_csv("PRC Data Breach Chronology - 1.13.20.csv")
+    plot_breaches_per_year(df1, df2)
 
 
 if __name__ == '__main__':
