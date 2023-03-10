3e7449b238228d5da0ca1327714ad388e0a3b84e
Tyrell Garza <tyrellgarza@t-MBP-14.local>
2023-03-09 23:33:33 -0800

    calculations and testing

--- cleanup.py
+++ cleanup.py
@@ -6,6 +6,35 @@ Data Breach Analysis: Investigating Trends and Impact on Businesses
 import pandas as pd
 from datetime import datetime
 from dateutil import relativedelta
+from typing import Tuple
+
+
+# research question 1
+def filter_by_year(
+        data: pd.DataFrame, start_year: int, end_year: int
+) -> pd.DataFrame:
+    '''
+
+    Vaidates accuracy/consistency data for question 1. Takes
+    dataframe and two integer arguements, filters dataframe
+    by year, returns resulting dataframe. (ensures data from
+    desired years).
+    '''
+    return data[data['year'].between(start_year, end_year)]
+
+
+def count_breaches_by_year(data: pd.DataFrame) -> Tuple[int, int]:
+    '''
+
+    Helps prove breach trend is increasing by calculating the
+    number of breaches in the earliest and latest year
+    of the dataset. See test for results.
+    '''
+    earliest_year = data['year'].min()
+    latest_year = data['year'].max()
+    breaches_in_earliest_year = data[data['year'] == earliest_year].shape[0]
+    breaches_in_latest_year = data[data['year'] == latest_year].shape[0]
+    return (breaches_in_earliest_year, breaches_in_latest_year)
 
 
 # research question 3
@@ -50,3 +79,32 @@ def clean_entities(data: pd.DataFrame) -> dict[str, int]:
             else:
                 unique[j] += 1
     return unique
+
+
+# research question 5
+def split_by_comma(data: pd.DataFrame, col_name: str) -> pd.DataFrame:
+    '''
+
+    Takes dataframe and column name as arguements. Splits column
+    values, creates new column, explodes into seperate rows and
+    returns resulting dataframe. Ensures accuracy of "Type of Breach'
+    values.
+    '''
+    return (
+        data.assign(**{col_name: data[col_name].str.split(', ')}).
+        explode(col_name)
+    )
+
+
+def total_individuals_affected(data: pd.DataFrame) -> pd.Series:
+    '''
+
+    Takes dataframe, returns series with total num of individuals
+    affected by each type of breach sorted in descending order.
+    Proving results of breach_individual_correlation. 
+    '''
+    return data.groupby(
+                        'Type_of_Breach'
+                        )[
+                          'Individuals_Affected'
+                          ].sum().sort_values(ascending=False)
--- test.py
+++ test.py
@@ -8,6 +8,33 @@ import pandas as pd
 from cse163_utils import assert_equals
 
 
+# research question 1 tests
+def test_filter_by_year(data: pd.DataFrame) -> None:
+    '''
+
+    Tests filter_by_year, returns dataframe with 7 rows,
+    year ranges from 2009 to 2017.
+    '''
+    filtered_data = cleanup.filter_by_year(data, 2009, 2017)
+    assert_equals(7, len(filtered_data))
+    assert_equals(2009, filtered_data['year'].min())
+    assert_equals(2014, filtered_data['year'].max())
+
+
+def test_breach_trend(data: pd.DataFrame) -> None:
+    '''
+
+    Tests that breach trend is increasing by using
+    count_breach_by_year to check number of breaches
+    in latest year (2013) > breaches in earliest year
+    (2009).
+    '''
+    earliest, latest = cleanup.count_breaches_by_year(data)
+    assert latest > earliest
+    assert earliest == 1
+    assert latest == 2
+
+
 def test_unique_loc(data: pd.DataFrame) -> None:
     assert_equals(6, len(cleanup.clean_entities(data)))
     assert_equals(1, cleanup.clean_entities(data)['E-mail'])
@@ -18,10 +45,40 @@ def test_average_response(data: pd.DataFrame) -> None:
     assert_equals(float(16), cleanup.clean_dates(data).loc[1, 'response_time'])
 
 
+# research question 5 tests
+def test_split_by_comma(data: pd.DataFrame) -> None:
+    '''
+
+    Tests the split_by_comma function. Returns a 
+    dataframe with 7 rows, where 'Type of Breach'
+    column contains correct values after splitting
+    by comma.
+    '''
+    exploded_data = cleanup.split_by_comma(data, 'Type_of_Breach')
+    assert_equals(7, len(exploded_data))
+    assert_equals('Theft', exploded_data.iloc[1]['Type_of_Breach'])
+    assert_equals('Theft', exploded_data.iloc[2]['Type_of_Breach'])
+
+
+def test_total_individuals_affected(data: pd.DataFrame) -> None:
+    '''
+
+    Tests total_individuals_affected method by proving
+    what visual showed (Theft affects most indivduals).
+    This method compares 'Theft' to other breach methods.
+    '''
+    total_affected = cleanup.total_individuals_affected(data)
+    assert total_affected['Theft'] > total_affected['Other']
+
+
 def main():
     test_data = pd.read_csv("test.csv")
     test_unique_loc(test_data)
     test_average_response(test_data)
+    test_filter_by_year(test_data)
+    test_split_by_comma(test_data)
+    test_total_individuals_affected(test_data)
+    test_breach_trend(test_data)
 
 
 if __name__ == '__main__':
